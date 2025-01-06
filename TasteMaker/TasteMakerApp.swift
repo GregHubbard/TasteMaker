d6f4b652f64d9a2694e7ebdec668d75804222907
GregHubbard <gdhubbardjr@gmail.com>
2025-01-06 14:58:03 -0500

    Initial Commit

---
+++ TasteMaker/TasteMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteMakerApp.swift
+//  TasteMaker
+//
+//  Created by Greg Hubbard on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TasteMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasteMakerTests/TasteMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasteMakerTests.swift
+//  TasteMakerTests
+//
+//  Created by Greg Hubbard on 1/6/25.
+//
+
+import Testing
+@testable import TasteMaker
+
+struct TasteMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
